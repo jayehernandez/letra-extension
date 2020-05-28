a0e76857c2b13e5b58243dd73327e22d23e62229
tbarland <tbarland77@hotmail.com>
2020-05-27 20:05:45 -0400

    add jest and supertest to the sever, add code coverage, and also add a example test to verify things are working correctly

---
+++ server/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: "node",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  "collectCoverage": true,
+  "collectCoverageFrom": ["**/**/*.js", "!**/coverage/**/*.js", "!**/**/jest.config.js"],
+  "coverageReporters": ["html", "text-summary"]
+};
