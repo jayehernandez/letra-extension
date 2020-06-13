0868ad8c8c4c85640d5ad80c9dd2b0f81322b569
Lint Action <letra.extension@gmail.com>
2020-06-13 02:22:53 +0000

    Fix code style issues with ESLint

--- server/test/letra.test.js
+++ server/test/letra.test.js
@@ -9,8 +9,7 @@ describe('/', () => {
     expect(res.statusCode).toEqual(200);
   });
   it('returns a message', async () => {
-    const expectedMessage =
-      'Hi There! Nothing much here, just a server for the letra extension!';
+    const expectedMessage = 'Hi There! Nothing much here, just a server for the letra extension!';
     const res = await request(app).get('/');
     expect(res.text).toEqual(expectedMessage);
   });
