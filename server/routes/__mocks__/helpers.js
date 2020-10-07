a237c3eb904692768eca333f80464a582ac83e5f
Adi Cohen <adico1@gmail.com>
2020-10-07 22:11:13 +0300

    refactor/cover lerna.js 100%

---
+++ server/routes/__mocks__/helpers.js
@@ -0,0 +1,21 @@
+const helpers = jest.createMockFromModule('./helpers');
+const consts = require('../../test/test-consts');
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+helpers.getRandomChoice = (options) => {
+  const randomIndex = 1;
+  return options[randomIndex];
+};
+helpers.getLanguage = () => {
+  const options = ['', consts.HELPERS_MOCK_RET_LANGUAGE];
+  return helpers.getRandomChoice(options);
+};
+helpers.getPhoto = async () => {
+  await delay(100);
+  return consts.UNSPLASH_API_RET_IMAGE;
+};
+
+module.exports = helpers;
