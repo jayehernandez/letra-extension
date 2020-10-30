e920772ff08afdf57b1f16c61192bfe9f1683df6
shehriyarnadeem <shehriyarnadeemy@gmail.com>
2020-10-30 16:12:51 +0500

    Fix lint Issue

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 global.fetch = require('node-fetch');
 
 const app = require('./app');
+
 const { port } = require('./config');
 
 const server = http.createServer(app);
