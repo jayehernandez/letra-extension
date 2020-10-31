8aec35fc55509ef018446c341b21b786521b052f
Matias Kinnunen <github@mtsknn.fi>
2020-10-31 16:45:40 +0200

    Fix spaces in Pug templates

--- client/src/views/Credit.vue
+++ client/src/views/Credit.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
 .bottom-left
-  | Photo by
-  a(:href="photoLink")  {{ photographerData.name }}
-  |  on
-  a(href="https://unsplash.com/?utm_source=letra&utm_medium=referral")  Unsplash
-  |  | Text to Speech provided by
-  a(href="https://responsivevoice.org/")  responsivevoice.org
+  | Photo by#{' '}
+  a(:href="photoLink") {{ photographerData.name }}
+  |  on#{' '}
+  a(href="https://unsplash.com/?utm_source=letra&utm_medium=referral") Unsplash
+  |  | Text to Speech provided by#{' '}
+  a(href="https://responsivevoice.org/") responsivevoice.org
 </template>
 
 <script>
