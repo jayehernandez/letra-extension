c41d0ff63a45268192d55f5184712ab5bd13625d
Jaye Hernandez <hernandez.jaye@gmail.com>
2022-10-21 00:57:39 +0800

    Fix product hunt pic not showing

--- site/components/About.js
+++ site/components/About.js
@@ -130,10 +130,9 @@ function About() {
             style={{ width: '250px', height: '54px', margin: 'auto' }}
             rel="noreferrer"
           >
-            <img
+            <Image
               src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=196571&theme=dark"
               alt="Letra - Passively learn a new language every time you open a new tab | Product Hunt"
-              style={{ width: '250px', height: '54px' }}
               width="250"
               height="54"
             />
--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
+    dangerouslyAllowSVG: true,
     domains: ['api.producthunt.com', 'avatars.githubusercontent.com', 'images.unsplash.com'],
   },
 };
